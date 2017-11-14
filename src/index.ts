121cfafa322200489c4802f7e9e6aed8840c0f99
sourabh.rustagi <sourabh.rustagi@hotmail.com>
2017-11-14 18:58:25 +0530

    remove unnecessary

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,26 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TxtImgComponent } from './txtimg.component';
-import { SampleDirective } from './sample.directive';
-import { SamplePipe } from './sample.pipe';
-import { SampleService } from './sample.service';
+
 
 export * from './txtimg.component';
-export * from './sample.directive';
-export * from './sample.pipe';
-export * from './sample.service';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    TxtImgComponent,
-    SampleDirective,
-    SamplePipe
+    TxtImgComponent
   ],
   exports: [
-    TxtImgComponent,
-    SampleDirective,
-    SamplePipe
+    TxtImgComponent
   ]
 })
 export class TxtImgModule {
   static forRoot(): ModuleWithProviders {
     return {
       ngModule: TxtImgModule,
-      providers: [SampleService]
+      providers: []
     };
   }
 }
